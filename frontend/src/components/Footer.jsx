8b147b5922fc09a1e9f11d082125e0052271048c
Pratchaya <pratchaya.tanapibulphol@gmail.com>
2025-01-25 00:37:19 +0700

    Create Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,77 +2,49 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faFacebook, faInstagram, faGithub } from "@fortawesome/free-brands-svg-icons";
+import "./css/Footer.css";
+import { Container } from "react-bootstrap";
 
 function Footer() {
   return (
-    <div className="Footer">
+    <div className="Footer" style={{ backgroundColor: "#231F20" }}>
       {/* ------------------------------------ FOOTER ------------------------------------- */}
-      <div className=" ">
-        <footer className="text-center text-lg-start text-dark" style={{ backgroundColor: "#ECEFF1" }}>
-          <section className="d-flex justify-content-between p-4 text-white" style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}>
-            <div className="me-5">
-              <span>Get connected with us on social networks:</span>
-            </div>
 
-            <div>
-              <a href="https://www.facebook.com/BenzPratchaya" className="text-white me-4">
-                <FontAwesomeIcon icon={faFacebook} />
-              </a>
-              <a href="https://www.instagram.com/benz_pratch/" className="text-white me-4">
-                <FontAwesomeIcon icon={faInstagram} />
-              </a>
-              <a href="https://github.com/BenzPratchaya" className="text-white me-4">
-                <FontAwesomeIcon icon={faGithub} />
-              </a>
-            </div>
-          </section>
-
-          <section className="">
-            <div className="container text-center text-md-start mt-5">
-              <div className="row mt-3">
-                <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                  <h5 className="text-uppercase fw-bold">Lorem Ipsum</h5>
-                  <hr className="mb-4 mt-0 d-inline-block mx-auto" style={{ width: "60px", backgroundColor: "#7c4dff", height: "2px" }} />
-                  <p>Neque porro quisquam est qui dolorem</p>
-                </div>
-                <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                  <h5 className="text-uppercase fw-bold">About</h5>
-                  <hr className="mb-4 mt-0 d-inline-block mx-auto" style={{ width: "60px", backgroundColor: "#7c4dff", height: "2px" }} />
-                  <p>
-                    <a href="/home" style={{ color: "#00000080" }}>
-                      home
-                    </a>
-                  </p>
-                  <p>
-                    <a href="/home" style={{ color: "#00000080" }}>
-                      home
-                    </a>
-                  </p>
-                  <p>
-                    <a href="/home" style={{ color: "#00000080" }}>
-                      home
-                    </a>
-                  </p>
-                </div>
-
-                <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                  <h5 className="text-uppercase fw-bold">Contact</h5>
-                  <hr className="mb-4 mt-0 d-inline-block mx-auto" style={{ width: "60px", backgroundColor: "#7c4dff", height: "2px" }} />
-                  <p>
-                    <FontAwesomeIcon icon={faEnvelope} /> pratchaya.tanapibulphol@gmail.com
-                  </p>
-                  <p>
-                    <FontAwesomeIcon icon={faPhone} /> +66 6 4145 3596
-                  </p>
-                </div>
+      <br />
+      <footer className="footer">
+        <Container>
+          <div className="footer-content">
+            <div className="footer-logo">
+              <div className="logo">
+                <img
+                  src="http://localhost:3000/images/logo-with-text-dark.png"
+                  alt="Logo"
+                  style={{
+                    height: "60px",
+                    width: "auto",
+                  }}
+                />
               </div>
             </div>
-          </section>
-          <div className="text-white text-center p-3" style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}>
-            © 2024 Copyright :<a className="text-white" href="/home">System</a>
+            <nav className="footer-nav" style={{ fontSize: "20px" }}>
+              <a href="#home">หน้าแรก</a>
+              <a href="#about">เกี่ยวกับเรา</a>
+              <a href="#contact">ติดต่อเรา</a>
+              <a href="#feedback">แนะนำ-ติชม</a>
+            </nav>
           </div>
-        </footer>
-      </div>
+          <br />
+          <br />
+          <div className="footer-bottom" style={{ fontSize: "16px" }}>
+            <p>© CTRL G</p>
+            <div className="footer-terms">
+              <a href="#privacy">นโยบายความเป็นส่วนตัว</a>
+              <a href="#terms">เงื่อนไขและข้อกำหนด</a>
+            </div>
+          </div>
+        </Container>
+      </footer>
+      <div className="footer-bar"></div>
       {/* ---------------------------------- END FOOTER ----------------------------------- */}
     </div>
   );
